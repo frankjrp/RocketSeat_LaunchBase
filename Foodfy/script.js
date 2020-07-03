c9557d4ff46f60ea1baada364449a423772fa986
frankjrp <frankjrp@gmail.com>
2020-07-03 04:58:19 -0300

    Ajustes no script

--- Foodfy/script.js
+++ Foodfy/script.js
@@ -1,17 +1,17 @@
 const modalOverlay = document.querySelector(".modal_overlay");
-const modal = document.querySelector(".modal");
 const cards = document.querySelectorAll(".card");
 
 for (let card of cards) {
     const imageId = card.id;
-    const foodName = card.querySelector(".card-content p#name").textContent;
-    const foodBy = card.querySelector(".card-content p#by").textContent;
+    const foodName = card.querySelector("p#name").textContent;
+    const foodBy = card.querySelector("p#by").textContent;
 
     card.addEventListener("click", () => {
         modalOverlay.classList.add("active");
-        modalOverlay.querySelector(".card-modal .image-container img").src = (`./assets/${imageId}.png`);
-        modalOverlay.querySelector(".card-modal .card-content p#name").textContent = foodName;
-        modalOverlay.querySelector(".card-modal .card-content p#by").textContent = foodBy;
+
+        modalOverlay.querySelector("img").src = (`./assets/${imageId}.png`);
+        modalOverlay.querySelector("p#name").textContent = foodName;
+        modalOverlay.querySelector("p#by").textContent = foodBy;        
 
     })
 }
