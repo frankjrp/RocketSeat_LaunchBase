9faf15b12d2cea4e8a01bfce2f3eb981bdc22c58
frankjrp <frankjrp@gmail.com>
2020-07-12 05:57:45 -0300

    Dados do Front para o Back-end
    Passando dados do Front-end para o Back-end com Query Strings.

--- Módulo_03/public/scripts.js
+++ Módulo_03/public/scripts.js
@@ -1,16 +1,9 @@
-const modalOverlay = document.querySelector(".modal-overlay");
 const cards = document.querySelectorAll(".card");
 
 for (let card of cards) {
     const videoId = card.id; /* Ou assim: card.getAttribute("id"); */
 
     card.addEventListener("click", function () {
-        modalOverlay.classList.add("active");
-        modalOverlay.querySelector("iframe").src = (`https://www.youtube.com/embed/${videoId}`);
+        window.location.href = `/video?id=${videoId}`
     })
 }
-
-document.querySelector(".close-modal").addEventListener("click", function () {
-    modalOverlay.classList.remove("active");
-    modalOverlay.querySelector("iframe").src = "";
-})
