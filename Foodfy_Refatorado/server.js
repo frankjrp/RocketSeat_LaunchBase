64ab2cefe091a6ac48bdc3b6a91ad07f43188440
frankjrp <frankjrp@gmail.com>
2020-07-14 05:16:00 -0300

    Inclusão de desafio
    Refatoramento do Foodfy - criação do servidor, conversão dos arquivos html para njk, criação das rotas.

---
+++ Foodfy_Refatorado/server.js
@@ -0,0 +1,44 @@
+const express = require('express')
+const nunjucks = require('nunjucks')
+
+const server = express()
+const recipes = require('./data')
+
+server.use(express.static('public'))
+
+server.set("view engine", "njk")
+
+nunjucks.configure("views", {
+    express: server,
+    autoescape: false,
+    noCache: true
+})
+
+server.get("/", function (req, res) {
+    return res.render("index", { recipes: recipes })
+})
+
+server.get("/about", function (req, res) {
+    const about = {
+        aboutTitle: "Sobre o Foodfy",
+        aboutDesc: `Suspendisse placerat neque neque. Morbi dictum nulla non sapien rhoncus, et mattis erat commodo. Aliquam vel lacus a justo mollis luctus. Proin vel auctor eros, sed eleifend nunc. Curabitur eget tincidunt risus. Mauris malesuada facilisis magna, vitae volutpat sapien tristique eu. Morbi metus nunc, interdum in erat placerat, aliquam iaculis massa. Duis vulputate varius justo pharetra maximus. In vehicula enim nec nibh porta tincidunt. Vestibulum at ultrices turpis, non dictum metus. Vivamus ligula ex, semper vitae eros ut, euismod convallis augue. <br><br> Fusce nec pulvinar nunc. Duis porttitor tincidunt accumsan. Quisque pulvinar mollis ipsum ut accumsan. Proin ligula lectus, rutrum vel nisl quis, efficitur porttitor nisl. Morbi ut accumsan felis, eu ultrices lacus. Integer in tincidunt arcu, et posuere ligula. Morbi cursus facilisis feugiat. Praesent euismod nec nisl at accumsan. Donec libero neque, vulputate semper orci et, malesuada sodales eros. Nunc ut nulla faucibus enim ultricies euismod.`,
+        startTitle: "Como tudo começou…",
+        startDesc: `Suspendisse placerat neque neque. Morbi dictum nulla non sapien rhoncus, et mattis erat commodo. Aliquam vel lacus a justo mollis luctus. Proin vel auctor eros, sed eleifend nunc. Curabitur eget tincidunt risus. Mauris malesuada facilisis magna, vitae volutpat sapien tristique eu. Morbi metus nunc, interdum in erat placerat, aliquam iaculis massa. Duis vulputate varius justo pharetra maximus. In vehicula enim nec nibh porta tincidunt. Vestibulum at ultrices turpis, non dictum metus. Vivamus ligula ex, semper vitae eros ut, euismod convallis augue. <br><br> Fusce nec pulvinar nunc. Duis porttitor tincidunt accumsan. Quisque pulvinar mollis ipsum ut accumsan. Proin ligula lectus, rutrum vel nisl quis, efficitur porttitor nisl. Morbi ut accumsan felis, eu ultrices lacus. Integer in tincidunt arcu, et posuere ligula. Morbi cursus facilisis feugiat. Praesent euismod nec nisl at accumsan. Donec libero neque, vulputate semper orci et, malesuada sodales eros. Nunc ut nulla faucibus enim ultricies euismod.`,
+        ourRecipesTitle: "Nossas receitas",
+        ourRecipesDesc: `Fusce nec pulvinar nunc. Duis porttitor tincidunt accumsan. Quisque pulvinar mollis ipsum ut accumsan. Proin ligula lectus, rutrum vel nisl quis, efficitur porttitor nisl. Morbi ut accumsan felis, eu ultrices lacus. Integer in tincidunt arcu, et posuere ligula. Morbi cursus facilisis feugiat. Praesent euismod nec nisl at accumsan. Donec libero neque, vulputate semper orci et, malesuada sodales eros. Nunc ut nulla faucibus enim ultricies euismod.`
+    }
+
+    return res.render("about", { about: about })
+})
+
+server.get("/recipes", function (req, res) {
+    return res.render("recipes", { recipes: recipes })
+})
+
+server.get("/recipe", function (req, res) {
+    return res.render("recipe")
+})
+
+server.listen(5000, function () {
+    console.log("Server is running")
+})
