475d44b852435fec40fa4293810ee31a0de06042
frankjrp <frankjrp@gmail.com>
2020-07-31 01:21:48 -0300

    Cards atualizados

--- Módulo_03/Desafio_03_3/data_courses.js
+++ Módulo_03/Desafio_03_3/data_courses.js
@@ -1,19 +1,25 @@
 module.exports = [
     {
         id: "starter",
-        description: "Rocketseat | Starter",
+        title: "Rocketseat | Starter",
+        highlight: "Torne-se um programador desejado",
+        description: "no mercado com esses cursos gratuitos",
         modules: "2 módulos",
         price: "Curso gratuito"
     },
     {
         id: "launchbase",
-        description: "Rocketseat | LaunchBase",
+        title: "Rocketseat | LaunchBase",
+        highlight: "Domine programação do zero",
+        description: "e tenha acesso às melhores oportunidades do mercado",
         modules: "8 módulos",
         price: "Curso pago"
     },
     {
         id: "gostack",
-        description: "Rocketseat | GoStack",
+        title: "Rocketseat | GoStack",
+        highlight: "Treinamento imersivo",
+        description: "nas tecnologias mais modernas de desenvolvimento web e mobile",
         modules: "10 módulos",
         price: "Curso pago"
     }
