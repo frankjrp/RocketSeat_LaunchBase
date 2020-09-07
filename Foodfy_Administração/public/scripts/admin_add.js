db554e966eb656684bc789233d82a42ef0bb2c9b
frankjrp <frankjrp@gmail.com>
2020-09-07 02:56:55 -0300

    Finalizado botão dinâmico

--- Foodfy_Administração/public/scripts/admin_add.js
+++ Foodfy_Administração/public/scripts/admin_add.js
@@ -1,37 +1,20 @@
-function addIngredient() {
-    const ingredients = document.querySelector("#add .inputs");
-    const fieldContainer = document.querySelectorAll("#add .ingredient");
+const inputs = document.querySelectorAll(".add")
 
-    // Realiza um clone do último ingrediente adicionado
-    const newField = fieldContainer[fieldContainer.length - 1].cloneNode(true);
+for (let input of inputs) {
+    const btn = input.querySelector("span")
 
-    // Não adiciona um novo input se o último tem um valor vazio
-    if (newField.children[0].value == "") return false;
+    btn.addEventListener("click", () => {
+        const fieldContainer = input.querySelectorAll(".addInput")
 
-    // Deixa o valor do input vazio
-    newField.children[0].value = "";
-    ingredients.appendChild(newField);
+        // Realiza um clone do último ingrediente adicionado
+        const newField = fieldContainer[fieldContainer.length - 1].cloneNode(true)
+
+        // Não adiciona um novo input se o último tem um valor vazio
+        if (newField.children[0].value == "") return false
+
+        // Deixa o valor do input vazio
+        newField.children[0].value = ""
+        input.querySelector(".inputs").appendChild(newField)
+    })
+
 }
-
-function addPreparation() {
-    const ingredients = document.querySelector("#prep .inputs");
-    const fieldContainer = document.querySelectorAll("#prep .preparation");
-
-    // Realiza um clone do último ingrediente adicionado
-    const newField = fieldContainer[fieldContainer.length - 1].cloneNode(true);
-
-    // Não adiciona um novo input se o último tem um valor vazio
-    if (newField.children[0].value == "") return false;
-
-    // Deixa o valor do input vazio
-    newField.children[0].value = "";
-    ingredients.appendChild(newField);
-}
-
-document
-    .querySelector("#add span")
-    .addEventListener("click", addIngredient);
-
-document
-    .querySelector("#prep span")
-    .addEventListener("click", addPreparation);
