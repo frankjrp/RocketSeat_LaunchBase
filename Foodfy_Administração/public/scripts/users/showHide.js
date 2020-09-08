4f47969aa2437dc5159e1e98207813f46f1f330f
frankjrp <frankjrp@gmail.com>
2020-09-08 04:15:50 -0300

    Desafio finalizado
    Realocação dos arquivos de style e script.

--- Foodfy_Administração/public/scripts/users/showHide.js
+++ Foodfy_Administração/public/scripts/users/showHide.js
@@ -1,13 +1,4 @@
-const recipes = document.querySelectorAll(".card")
 const topics = document.querySelectorAll(".topic")
-
-for (let i = 0; i < recipes.length; i++) {
-    const recipeId = i
-
-    recipes[i].addEventListener("click", () => {
-        window.location.href = `/recipes/${recipeId}`
-    })
-}
 
 for (let topic of topics) {
     const btn = topic.querySelector("span")
