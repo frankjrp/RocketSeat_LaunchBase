a102a4f798cd8dd8ed96db600066f23d66650137
frankjrp <frankjrp@gmail.com>
2020-09-09 01:48:53 -0300

    Desafio finalizado
    Ajustes finais nas rotas.

--- Foodfy_Administração/routes.js
+++ Foodfy_Administração/routes.js
@@ -1,16 +1,16 @@
 const express = require('express')
 const routes = express.Router()
-const recipes = require('./controllers/users')
+const users = require('./controllers/users')
 const admins = require('./controllers/admins')
 
 routes.get('/', function (req, res) {
     return res.redirect("/index")
 })
 
-routes.get('/index', recipes.index)
-routes.get('/recipes', recipes.recipes)
-routes.get('/recipes/:index', recipes.show)
-routes.get('/about', recipes.about)
+routes.get('/index', users.index)
+routes.get('/recipes', users.recipes)
+routes.get('/recipes/:index', users.show)
+routes.get('/about', users.about)
 
 routes.get('/admin/recipes', admins.index)
 routes.get('/admin/recipes/create', admins.create)
