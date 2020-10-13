2869611d58856666e200c904de143e060d06da1e
frankjrp <frankjrp@gmail.com>
2020-10-13 04:21:39 -0300

    Configurações para total de instrutores e contagem de páginas

--- Módulo_05/public/scripts.js
+++ Módulo_05/public/scripts.js
@@ -8,21 +8,50 @@ for (item of menuItems) {
 }
 
 //Paginação
-let totalPages = 20,
-    selectedPage = 15,
-    pages = []
+function paginate(selectedPage, totalPages) {
+    let pages = [],
+        oldPage
 
-for (let currentPage = 1; currentPage <= totalPages; currentPage++) {
+    for (let currentPage = 1; currentPage <= totalPages; currentPage++) {
 
-    const firstAndLastPage = currentPage == 1 || currentPage == totalPages
-    const pagesAfterSelectePage = currentPage <= selectedPage + 2
-    const pagesBeforeSelectePage = currentPage >= selectedPage - 2
+        const firstAndLastPage = currentPage == 1 || currentPage == totalPages
+        const pagesAfterSelectePage = currentPage <= selectedPage + 2
+        const pagesBeforeSelectePage = currentPage >= selectedPage - 2
 
-    if (firstAndLastPage || pagesBeforeSelectePage && pagesAfterSelectePage) {
-        pages.push(currentPage)
+        if (firstAndLastPage || pagesBeforeSelectePage && pagesAfterSelectePage) {
+
+            if (oldPage && currentPage - oldPage > 2) {
+                pages.push("...")
+            }
+
+            if (oldPage && currentPage - oldPage == 2) {
+                pages.push(oldPage + 1)
+            }
+
+            pages.push(currentPage)
+
+            oldPage = currentPage
+        }
 
     }
 
-    console.log(pages)
-
+    return pages
 }
+
+const pagination = document.querySelector(".pagination")
+const page = +pagination.dataset.page
+const total = +pagination.dataset.total
+const pages = paginate(page, total)
+
+let elements = ""
+
+for (let page of pages) {
+    if (String(page).includes("...")) {
+        elements += `<span>${page}</span>`
+
+    } else {
+        elements += `<a href="?page=${page}">${page}</a>`
+    }
+}
+
+pagination.innerHTML = elements
