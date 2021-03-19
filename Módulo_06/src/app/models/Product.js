cafc5926a711ecbeeadac553fcbd48a1d42ff39e
frankjrp <frankjrp@gmail.com>
2021-03-19 04:27:15 -0300

    Configurações dos botões de inclusão, edição e exclusão

--- Módulo_06/src/app/models/Product.js
+++ Módulo_06/src/app/models/Product.js
@@ -16,18 +16,54 @@ module.exports = {
             RETURNING id
         `
 
+        data.price = data.price.replace(/\D/g, "")
+
         const values = [
             data.category_id,
-            1,
+            data.user_id || 1,
+            data.name,
+            data.description,
+            data.old_price || data.price,
+            data.price,
+            data.quantity,
+            data.status || 1
+        ]
+
+        return db.query(query, values)
+
+    },
+    find(id) {
+        return db.query('SELECT * from products WHERE id = $1', [id])
+    },
+    update(data) {
+        const query = `
+            UPDATE products SET
+                category_id=($1),
+                user_id=($2),
+                name=($3),
+                description=($4),
+                old_price=($5),
+                price=($6),
+                quantity=($7),
+                status=($8)
+            WHERE id = $9
+        `
+
+        const values = [
+            data.category_id,
+            data.user_id,
             data.name,
             data.description,
             data.old_price,
             data.price,
             data.quantity,
-            data.status            
+            data.status,
+            data.id
         ]
 
         return db.query(query, values)
-
+    },
+    delete(id) {
+        return db.query('DELETE from products WHERE id = $1', [id])
     }
 }
