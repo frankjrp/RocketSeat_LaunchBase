64de869d09889c690d479a65584c394842ba9824
frankjrp <frankjrp@gmail.com>
2021-03-31 04:37:11 -0300

    Inclusão dos desafios 1.1 e 1.2

---
+++ Módulo_06/Desafio_06_1_1/database.sql
@@ -0,0 +1,85 @@
+CREATE TABLE "customers" (
+  "id" SERIAL PRIMARY KEY,
+  "name" text,
+  "cpf" text,
+  "address" text,
+  "email" text,
+  "phone" text,
+  "order_id" int
+);
+
+CREATE TABLE "agencies" (
+  "id" SERIAL PRIMARY KEY,
+  "cnpj" text,
+  "email" text,
+  "phone" text,
+  "manager_id" int UNIQUE,
+  "address_id" int UNIQUE,
+  "order_id" int
+);
+
+CREATE TABLE "managers" (
+  "id" SERIAL PRIMARY KEY,
+  "name" text NOT NULL,
+  "cpf" text,
+  "age" int
+);
+
+CREATE TABLE "addresses" (
+  "id" SERIAL PRIMARY KEY,
+  "street" text,
+  "district" text,
+  "number" int,
+  "city" text,
+  "state" text,
+  "country" text
+);
+
+CREATE TABLE "cars" (
+  "id" SERIAL PRIMARY KEY,
+  "license_plate" text,
+  "color" text,
+  "chassi" text,
+  "model_id" int
+);
+
+CREATE TABLE "models" (
+  "id" SERIAL PRIMARY KEY,
+  "brand" text,
+  "model" text,
+  "manufacture_year" int
+);
+
+CREATE TABLE "orders" (
+  "id" SERIAL PRIMARY KEY,
+  "created_at" timestamp DEFAULT (now()),
+  "customer_id" int,
+  "car_id" int,
+  "agency_id" int
+);
+
+CREATE TABLE "car_order" (
+  "id" SERIAL PRIMARY KEY,
+  "car_id" int,
+  "order_id" int
+);
+
+ALTER TABLE "managers" ADD FOREIGN KEY ("id") REFERENCES "agencies" ("manager_id");
+
+ALTER TABLE "addresses" ADD FOREIGN KEY ("id") REFERENCES "agencies" ("address_id");
+
+ALTER TABLE "cars" ADD FOREIGN KEY ("model_id") REFERENCES "models" ("id");
+
+ALTER TABLE "customers" ADD FOREIGN KEY ("order_id") REFERENCES "orders" ("id");
+
+ALTER TABLE "agencies" ADD FOREIGN KEY ("order_id") REFERENCES "orders" ("id");
+
+ALTER TABLE "car_order" ADD FOREIGN KEY ("car_id") REFERENCES "cars" ("id");
+
+ALTER TABLE "car_order" ADD FOREIGN KEY ("order_id") REFERENCES "orders" ("id");
+
+ALTER TABLE "orders" ADD FOREIGN KEY ("customer_id") REFERENCES "customers" ("id");
+
+ALTER TABLE "orders" ADD FOREIGN KEY ("car_id") REFERENCES "cars" ("id");
+
+ALTER TABLE "orders" ADD FOREIGN KEY ("agency_id") REFERENCES "agencies" ("id");
---
+++ Módulo_06/Desafio_06_1_2/data_courses.js
@@ -0,0 +1,26 @@
+module.exports = [
+    {
+        id: "starter",
+        title: "Rocketseat | Starter",
+        highlight: "Torne-se um programador desejado",
+        description: "no mercado com esses cursos gratuitos",
+        modules: "2 módulos",
+        price: "Curso gratuito"
+    },
+    {
+        id: "launchbase",
+        title: "Rocketseat | LaunchBase",
+        highlight: "Domine programação do zero",
+        description: "e tenha acesso às melhores oportunidades do mercado",
+        modules: "8 módulos",
+        price: "Curso pago"
+    },
+    {
+        id: "gostack",
+        title: "Rocketseat | GoStack",
+        highlight: "Treinamento imersivo",
+        description: "nas tecnologias mais modernas de desenvolvimento web e mobile",
+        modules: "10 módulos",
+        price: "Curso pago"
+    }
+]
