51a3f175942091cc3c430134f6ac26b3cd1acc29
frankjrp <frankjrp@gmail.com>
2021-09-19 03:06:14 -0300

    Ajustes na exibição das imagens

--- Foodfy_Upload_Imagens/public/scripts/users/scripts.js
+++ Foodfy_Upload_Imagens/public/scripts/users/scripts.js
@@ -13,3 +13,16 @@ for (let topic of topics) {
         }
     })
 }
+
+const ImageGallery = {
+    highlight: document.querySelector('.gallery .highlight > img'),
+    previews: document.querySelectorAll('.gallery-preview img'),
+    setImage(e) {
+        const { target } = e
+
+        ImageGallery.previews.forEach(preview => preview.classList.remove('active'))
+        target.classList.add('active')
+
+        ImageGallery.highlight.src = target.src
+    }
+}
